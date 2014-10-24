1fd610c142fbe5d5a5c451d8e07d42c59495581c
sota1235 <t11460ss@sfc.keio.ac.jp>
2014-10-24 15:41:12 +0900

    fix config.rb | 少し修正

--- config.rb
+++ config.rb
@@ -8,10 +8,10 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "/:year/:month/:day/:title.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
+  blog.taglink = "tags/:tag.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
